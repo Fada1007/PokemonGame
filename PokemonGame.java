77256657ec8976112dd45fe3721bce0339e8463d
สุนันทา ด่าสะอิ <43301253+Sunantha357@users.noreply.github.com>
2020-02-09 22:15:28 +0700

    Add files via upload

---
+++ PokemonGame.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemongame;
+import java.io.*;
+/**
+ *
+ * @author asus
+ */
+public class PokemonGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      int i ;
+         Pokemon Charecter = new Pokemon() ;
+         System.out.println(" Your're welcome");
+         Charecter.setName();
+         Charecter.showStatus();
+         Charecter.startGame();
+        // TODO code application logic here
+    }
+    
+}
